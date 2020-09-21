5a1cc8b81732e9a9ffd903aca66ee9b6cfc39f8d
Mateusz Zaremba <mat.zar@icloud.com>
2020-09-21 15:02:33 +0100

    add different templates

--- bootstrap-init/src/app/servers/servers.component.ts
+++ bootstrap-init/src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  // can be replaced by
+  // template: '<app-server></app-server><app-server></app-server>', // to write html code directly in here <- use this for one-line inline htmls
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`, // <- use backticks for multi-line html inlines (it's JavaScript template expressions)
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
