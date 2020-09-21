ad4faa89209c17c72f1fe52e49002987010f849d
Mateusz Zaremba <mat.zar@icloud.com>
2020-09-21 16:27:36 +0100

    added more css styling to success-alert and warning-alert

--- bootstrap-init/src/app/warning-alert/warning-alert.component.ts
+++ bootstrap-init/src/app/warning-alert/warning-alert.component.ts
@@ -4,12 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-warning-alert',
   template: `
     <hr>
-    <h3>WARNING!<\h3>
+    <h3>WARNING!</h3>
     <hr>
   `,
   styles: [`
     h3 {
       color: red;
+      padding: 20px;
+      background-color: mistyrose;
+      border: 1px solid red;
     }
   `]
 })
