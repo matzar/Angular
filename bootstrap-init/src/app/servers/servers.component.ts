9b8f8d29dd2a8d65d6d96b4ff05ad3674f05d3d5
Mateusz Zaremba <mat.zar@icloud.com>
2020-09-21 15:18:38 +0100

    comments

--- bootstrap-init/src/app/servers/servers.component.ts
+++ bootstrap-init/src/app/servers/servers.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //* selector can be omitted
+  //? selector can be omitted
   selector: 'app-servers',
-  //! template is compulsory
+  //! templateUrl/template is compulsory
   // templateUrl: './servers.component.html',
   // can be replaced by
   // template: '<app-server></app-server><app-server></app-server>', // to write html code directly in here <- use this for one-line inline htmls
   template: `
   <app-server></app-server>
   <app-server></app-server>`, // <- use backticks for multi-line html inlines (it's JavaScript template expressions)
-  //* styleUrls can be omitted
+  //? styleUrls/styles can be omitted
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
