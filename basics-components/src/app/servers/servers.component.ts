9a2afbf888072d8911980390e547a6d14fe688cf
Mateusz Zaremba <mat.zar@icloud.com>
2020-11-19 10:35:43 +0000

    app-server

--- basics-components/src/app/servers/servers.component.ts
+++ basics-components/src/app/servers/servers.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  template: `<app-server></app-server>`,
+  styleUrls: ["./servers.component.css"],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
