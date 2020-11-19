7c017aea469a6074bb9b729d6a99c904fcc627ad
Mateusz Zaremba <mat.zar@icloud.com>
2020-11-19 10:23:34 +0000

    redone basic-components assignemnt

--- basics-components/src/app/app.module.ts
+++ basics-components/src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
